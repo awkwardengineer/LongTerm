d0a95ecb2ea5ef84bd8c4f102673f21a17506f1a
Sam <sam@awkwardengineer.com>
2013-08-13 00:39:19 -0400

    calendar reads data and renders

---
+++ scripts/dataset.js
@@ -0,0 +1,370 @@
+var set1 = {
+ "summary": "Sam Feller",
+ "items": [
+  {
+   "summary": "Kayla's Birthday",
+   "start": {
+    "date": "2008-09-18"
+   },
+   "end": {
+    "date": "2008-09-19"
+   }
+  },
+  {
+   "summary": "Running Club",
+   "start": {
+    "dateTime": "2011-01-31T18:45:00-05:00",
+    "timeZone": "America/New_York"
+   },
+   "end": {
+    "dateTime": "2011-01-31T19:45:00-05:00",
+    "timeZone": "America/New_York"
+   }
+  },
+  {
+   "summary": "Eliza's wedding!",
+   "start": {
+    "date": "2013-08-23"
+   },
+   "end": {
+    "date": "2013-08-26"
+   }
+  },
+  {
+   "summary": "dentist",
+   "start": {
+    "dateTime": "2013-08-15T08:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-15T09:00:00-04:00"
+   }
+  },
+  {
+   "summary": "\"Geek Lunch\" at WPI",
+   "start": {
+    "dateTime": "2014-02-12T12:00:00-05:00"
+   },
+   "end": {
+    "dateTime": "2014-02-12T13:00:00-05:00"
+   }
+  },
+  {
+   "summary": "Marion Trip?",
+   "start": {
+    "date": "2013-08-10"
+   },
+   "end": {
+    "date": "2013-08-12"
+   }
+  },
+  {
+   "summary": "smoothie dinner",
+   "start": {
+    "dateTime": "2013-08-06T19:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-06T20:00:00-04:00"
+   }
+  },/*
+  {
+   "summary": "call Cathy at spy museum",
+   "start": {
+    "dateTime": "2013-08-06T10:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-06T11:00:00-04:00"
+   }
+  },
+  {
+   "summary": "Call roberta",
+   "start": {
+    "dateTime": "2013-08-02T12:30:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-02T13:30:00-04:00"
+   }
+  },
+  {
+   "summary": "meet Slava",
+   "start": {
+    "dateTime": "2013-08-08T17:30:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-08T19:00:00-04:00"
+   }
+  },
+  {
+   "summary": "call cathy",
+   "start": {
+    "dateTime": "2013-08-06T16:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-06T16:30:00-04:00"
+   }
+  },
+  {
+   "summary": "follow up on NDAs",
+   "start": {
+    "dateTime": "2013-08-07T14:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-07T14:30:00-04:00"
+   }
+  },
+  {
+   "summary": "greentown labs",
+   "start": {
+    "dateTime": "2013-08-12T18:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-12T19:00:00-04:00"
+   }
+  },*/
+  {
+   "summary": "FOOSBALL!",
+   "start": {
+    "dateTime": "2013-08-26T18:30:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-26T20:00:00-04:00"
+   }
+  },/*
+  {
+   "summary": "add webform for the europeans",
+   "start": {
+    "dateTime": "2013-08-14T18:30:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-14T21:30:00-04:00"
+   }
+  },
+  {
+   "summary": "add webform for the europeans",
+   "start": {
+    "dateTime": "2013-08-13T18:30:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-13T21:30:00-04:00"
+   }
+  }*/
+ ]
+};
+
+var set2 ={
+ "summary": "sarahsekine@gmail.com",
+ "items": [
+  {
+   "summary": "Matt settler's birthday",
+   "start": {
+    "date": "2012-10-22"
+   },
+   "end": {
+    "date": "2012-10-23"
+   }
+  },
+  {
+   "summary": "Eliza's wedding!",
+   "start": {
+    "date": "2013-08-23"
+   },
+   "end": {
+    "date": "2013-08-26"
+   }
+  },
+  {
+   "summary": "Rosh Hashanah",
+   "start": {
+    "date": "2013-09-04"
+   },
+   "end": {
+    "date": "2013-09-07"
+   }
+  },
+  {
+   "summary": "Yom Kippur",
+   "start": {
+    "date": "2013-09-13"
+   },
+   "end": {
+    "date": "2013-09-15"
+   }
+  },
+  {
+   "summary": "Hawaii",
+   "start": {
+    "date": "2013-10-10"
+   },
+   "end": {
+    "date": "2013-10-21"
+   }
+  },
+  {
+   "summary": "Yoga SA",
+   "start": {
+    "dateTime": "2013-06-07T18:15:00-04:00",
+    "timeZone": "America/New_York"
+   },
+   "end": {
+    "dateTime": "2013-06-07T20:45:00-04:00",
+    "timeZone": "America/New_York"
+   }
+  },
+  {
+   "summary": "PCMH Training in Austin",
+   "start": {
+    "dateTime": "2013-09-09T07:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-09-10T12:30:00-04:00"
+   }
+  },
+  {
+   "summary": "Haircut",
+   "start": {
+    "dateTime": "2013-08-31T10:00:00-04:00",
+    "timeZone": "America/New_York"
+   },
+   "end": {
+    "dateTime": "2013-08-31T11:00:00-04:00",
+    "timeZone": "America/New_York"
+   }
+  },
+  {
+   "summary": "Eyebrows",
+   "start": {
+    "dateTime": "2013-09-21T10:00:00-04:00",
+    "timeZone": "America/New_York"
+   },
+   "end": {
+    "dateTime": "2013-09-21T11:00:00-04:00",
+    "timeZone": "America/New_York"
+   }
+  },
+  {
+   "summary": "Dinner at Damaris'",
+   "start": {
+    "dateTime": "2013-08-08T18:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-08T19:00:00-04:00"
+   }
+  },
+  {
+   "summary": "PCMH training  in Austin",
+   "start": {
+    "dateTime": "2013-09-08T10:30:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-09-11T11:30:00-04:00"
+   }
+  },
+  {
+   "summary": "Go Ape",
+   "start": {
+    "dateTime": "2013-11-24T13:00:00-05:00"
+   },
+   "end": {
+    "dateTime": "2013-11-24T16:00:00-05:00"
+   }
+  },
+  {
+   "summary": "Book Go Ape for Sunday after Thanksgiving.",
+   "start": {
+    "dateTime": "2013-10-25T11:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-10-25T12:00:00-04:00"
+   }
+  },
+  {
+   "summary": "HOLD GA Trip for work",
+   "start": {
+    "dateTime": "2013-08-12T08:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-16T09:00:00-04:00"
+   }
+  },
+  {
+   "summary": "Dr. Appointment",
+   "start": {
+    "dateTime": "2013-09-17T11:15:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-09-17T12:15:00-04:00"
+   }
+  },
+  {
+   "summary": "Dentist Appointment- will call to reschedule",
+   "start": {
+    "dateTime": "2013-08-06T09:30:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-06T10:30:00-04:00"
+   }
+  },
+  {
+   "summary": "Boston Restaurant Week",
+   "start": {
+    "date": "2013-08-18"
+   },
+   "end": {
+    "date": "2013-08-24"
+   }
+  },
+  {
+   "summary": "Boston Restaurant Week",
+   "start": {
+    "date": "2013-08-25"
+   },
+   "end": {
+    "date": "2013-08-31"
+   }
+  },
+  {
+   "summary": "San Fran",
+   "start": {
+    "date": "2013-10-04"
+   },
+   "end": {
+    "date": "2013-10-11"
+   }
+  },
+  {
+   "summary": "JetBlue flight 833 from Boston-> San Fran",
+   "start": {
+    "dateTime": "2013-10-04T19:37:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-10-05T02:08:00-04:00"
+   }
+  },
+  {
+   "summary": "Burlington, VT Trip",
+   "start": {
+    "date": "2013-08-30"
+   },
+   "end": {
+    "date": "2013-09-02"
+   }
+  },
+  {
+   "summary": "Dinner Date>?",
+   "start": {
+    "dateTime": "2013-08-22T18:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-22T20:00:00-04:00"
+   }
+  },
+  {
+   "summary": "Girls Hiking Trip",
+   "start": {
+    "dateTime": "2013-08-17T09:00:00-04:00"
+   },
+   "end": {
+    "dateTime": "2013-08-17T17:00:00-04:00"
+   }
+  }
+ ]
+}
--- scripts/renderCal.js
+++ scripts/renderCal.js
@@ -1,11 +1,11 @@
 $(document).ready( function(){
 
+
   drawCalendar();
   drawWeekendBars();
  
-
-
-
+  renderEvents( getData() );
+  
 });
 
 
@@ -14,15 +14,15 @@ function drawWeekendBars(){
   var OFFSET = 21.5;
   var WEEKSIZE = 21;
 
-  var unit = $("#testMeasure").width();
+  var UNIT = $("#testMeasure").width();
 
-  $(".weekend").css("width", 6 * unit );
+  $(".weekend").css("width", 6 * UNIT );
   
-  $("#week1").css("left", OFFSET * unit);
-  $("#week2").css("left", (OFFSET + WEEKSIZE) * unit);
-  $("#week3").css("left", (OFFSET + 2 * WEEKSIZE) * unit);
-  $("#week4").css("left", (OFFSET + 3 * WEEKSIZE) * unit);
-  $("#week5").css("left", (OFFSET + 4 * WEEKSIZE) * unit);
+  $("#week1").css("left", OFFSET * UNIT);
+  $("#week2").css("left", (OFFSET + WEEKSIZE) * UNIT);
+  $("#week3").css("left", (OFFSET + 2 * WEEKSIZE) * UNIT);
+  $("#week4").css("left", (OFFSET + 3 * WEEKSIZE) * UNIT);
+  $("#week5").css("left", (OFFSET + 4 * WEEKSIZE) * UNIT);
 
 }
 
@@ -83,5 +83,67 @@ function drawCalendar(){
   
   }
 
+}
 
+function getData()
+{
+  return set2;
+}
+
+function renderEvents(data){
+  //expects a JSON object using google's RESTful calendar API v3
+  var startDay
+  var endDay
+  
+  
+  var OFFSET = .5;
+  var OFFSET_Y = 1.05;
+
+
+  var UNIT = $("#testMeasure").width();
+  var UNIT_Y = $("#testMeasure").height() * .75;
+
+  
+  
+  for (item in data.items){
+  
+    //parses the JSON for dates
+    if ('date' in data.items[item].start){
+      
+      startDay = new Date(data.items[item].start.date + "T00:00:00-0500");
+      endDay = new Date(data.items[item].end.date + "T00:00:00-0500");
+      
+    }
+    else if ('dateTime' in data.items[item].start){
+     
+      startDay = new Date(data.items[item].start.dateTime);
+      endDay = new Date(data.items[item].end.dateTime);
+
+    }
+    
+    //calculate X position in units of days of calendar space
+    var x = startDay.getDate();
+    console.log("x: " + data.items[item].summary + " " + startDay);
+    
+    startDay.setDate(1);
+    if (startDay.getDay() == 0){
+      x = x + 7;
+    }
+    else{
+      x = x + startDay.getDay();
+    }
+    
+    
+    var today = new Date();
+    var y = startDay.getMonth() - today.getMonth() + ( startDay.getYear() - today.getYear() ) * 12;
+    console.log("y: " + y )
+    
+    if (y >= 0) //meaning the month is not in the past
+    {
+
+    $("<div class='event'><div class='summary'>" + data.items[item].summary + "</div></div>").css(   {"left" : UNIT * (OFFSET + 3 * x) ,"top" : UNIT_Y * (OFFSET_Y + y), "width" : 3 * UNIT } ).appendTo(".wrap") ;
+    
+    }
+  
+  }
 }
