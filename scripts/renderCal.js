0883e48bac872cd0a7f2ae1aeed9889721b7abe3
Sam <sam@awkwardengineer.com>
2013-08-22 16:44:59 -0400

    draws the calendar with SVG now!

--- scripts/renderCal.js
+++ scripts/renderCal.js
@@ -1,15 +1,159 @@
 $(document).ready( function(){
 
 
-  drawCalendar();
-  drawWeekendBars();
+  renderCal.init();
   
-  //right now, the code is written in a very bad way
-  //renderEvents( getData() );
+  testDate = new Date();
+  
+  renderCal.drawEvent(testDate,2,"blah blah");
+  
+  testDate.setMonth(7);
+  testDate.setDate(8);
+  renderCal.drawEvent(testDate,4,"blah blah");
+  
+  testDate.setMonth(12);
+  testDate.setDate(3);
+  renderCal.drawEvent(testDate,2,"blah blah");
+  
+  testDate.setMonth(1);
+  testDate.setDate(2);
+  renderCal.drawEvent(testDate,5,"blah blah");
+  
   
 });
 
+var renderCal = {
 
+  width: 800,
+  height: 600,
+  
+  unitX: 17.8,
+  unitY: 42.8,
+
+  measure: function(){
+    this.width = .9 * $(window).width();
+    this.height =.9 * $(window).height();
+  },
+  
+  init:function(){
+    this.measure();
+    this.unitX = this.width / 40;
+    this.unitY = this.height / 14;
+    
+    this.drawField();  
+    
+    console.log("initialized...")
+    console.log("height: " + this.height + " width: " + this.width + " unitX: " + this.unitX + " unitY: " + this.unitY);
+  },
+  
+  drawEvent:function(startDate, length,summary){
+  
+    var today = new Date();
+    var day = startDate.getDate();
+    var month = ( startDate.getMonth() - today.getMonth() + 12) % 12 + 1 ;
+    
+    startDate.setDate(1);
+    leading = startDate.getDay();
+    if (leading == 0) leading = 7;
+    
+    d3.select("#field").append("rect").attr("width", this.unitX * (.25 + length))
+                                        .attr("height", .8*this.unitX)
+                                        .attr("x", (day + leading - 1.625) * this.unitX)
+                                        .attr("y", this.unitY * month - .17*this.unitX)
+                                        .attr("rx", .4*this.unitX)
+                                        .attr("ry", .2*this.unitX)
+                                        .attr("class","event");
+                                        
+    d3.select("#field").append("text").text(summary)
+                                  .attr("x", (day + leading - 1.625 + .5*(.25 + length)) * this.unitX)
+                                  .attr("y", this.unitY * month - .17*this.unitX)
+                                  .attr("class","summary");
+  
+  },
+  
+  drawField:function(){
+       
+    var MONTHS = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT","NOV","DEC"];
+    var NUMDAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var DAYWEEK = ["M", "T", "W", "H", "F", "S", "S"];
+
+    var today = new Date();
+    
+    d3.select("#calendar").append("svg").attr("width", this.width).attr("height", this.height).attr("id", "field");
+    
+    for (var i =0; i <35; i++){
+    
+      d3.select("#field").append("text").text(DAYWEEK[ i % 7 ])
+                                  .attr("x", (i + 1) * this.unitX)
+                                  .attr("y", "1em" )
+                                  .attr("class","date");
+      if (i % 7 == 6){
+        d3.select("#field").append("rect").attr("width", .9*this.unitX )
+                                        .attr("height", 13.5 * this.unitY)
+                                        .attr("x", (i-.505) * this.unitX)
+                                        .attr("y", 0)
+                                        .attr("rx", .5*this.unitX)
+                                        .attr("ry", .5*this.unitX)
+                                        .attr("class","weekend");
+        d3.select("#field").append("rect").attr("width", .9*this.unitX )
+                                        .attr("height", 13.5 * this.unitY)
+                                        .attr("x", (i+.505) * this.unitX)
+                                        .attr("y", 0)
+                                        .attr("rx", .5*this.unitX)
+                                        .attr("ry", .5*this.unitX)
+                                        .attr("class","weekend");
+      }
+    }
+    
+    for (var i = 1; i <=12; i++){
+    
+      //add leading blanks so the month starts on the correct day of the week.
+      //works by setting the date to the first of the month and asking what day of the week it is.
+      
+      
+      
+      
+      
+      today.setDate(1);
+      
+      //the days are numbered 0-6, with Sunday as 0, but my calendar starts on monday
+      var leading = today.getDay();
+      if (leading == 0) leading = 7;
+      
+      d3.select("#field").append("rect").attr("width", this.unitX * (.25 + NUMDAYS[today.getMonth()]))
+                                        .attr("height", .5*this.unitX)
+                                        .attr("x", (leading - .5) * this.unitX)
+                                        .attr("y", this.unitY * i)
+                                        .attr("rx", .25*this.unitX)
+                                        .attr("ry", .25*this.unitX)
+                                        .attr("class","month");
+      d3.select("#field").append("text").text(MONTHS[today.getMonth()])
+                                        .attr("x", leading * this.unitX)
+                                        .attr("y", this.unitY * i)
+                                        .attr("class","date");
+                                        
+      for (var j = 0; j < NUMDAYS[today.getMonth()]; j++){
+            d3.select("#field").append("text").text(j + 1)
+                                              .attr("x", (j + leading) * this.unitX)
+                                              .attr("y", i * this.unitY + .33*this.unitX)
+                                              .attr("class","date");
+      }
+      
+      today.setMonth(today.getMonth() + 1);
+      
+    }
+    
+  
+  }
+  
+  
+  
+
+};
+
+
+
+/*
 
 function drawWeekendBars(){
   var OFFSET = 27;
@@ -185,4 +329,4 @@ function render(item)
   }
   
 }
-  
+*/  
